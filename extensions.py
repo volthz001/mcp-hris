0775d9e8bc10e64b6b2b46eeff22bf709ae6a7f1
Hizkia siallagan <Hizkiasiallagan001@gmail.com>
2026-07-07 19:33:45 +0700

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -1,3 +1,18 @@
+# extensions.py
 from flask_pymongo import PyMongo
 
 mongo = PyMongo()
+
+def get_current_user():
+    """Ambil dokumen user aktif dari MongoDB (diakses melalui session)."""
+    from flask import session
+    from bson.objectid import ObjectId
+    
+    uid = session.get("user_id")
+    if not uid:
+        return None
+    try:
+        # Menggunakan mongo yang sudah di-import dari extensions
+        return mongo.db.users.find_one({"_id": ObjectId(uid)})
+    except Exception:
+        return None
